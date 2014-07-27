5d3ca65b2034549ec9082f4d88daeef7566b6c78
Andreas Lind <andreas@one.com>
2014-07-27 14:19:06 +0200

    Initial commit, 0.1.0.

---
+++ README.md
@@ -0,0 +1,70 @@
+express-extractheaders
+======================
+[![NPM version](https://badge.fury.io/js/express-extractheaders.png)](http://badge.fury.io/js/express-extractheaders)
+[![Build Status](https://travis-ci.org/papandreou/express-extractheaders.png?branch=master)](https://travis-ci.org/papandreou/express-extractheaders)
+[![Coverage Status](https://coveralls.io/repos/papandreou/express-extractheaders/badge.png)](https://coveralls.io/r/papandreou/express-extractheaders)
+[![Dependency Status](https://david-dm.org/papandreou/express-extractheaders.png)](https://david-dm.org/papandreou/express-extractheaders)
+
+Express middleware that allows you to specify your HTTP response headers inside the `<head>` of your HTML as `<meta http-equiv="..." content="...">` tags. The primary use case is static HTML, but the middleware works no matter what's actually generating the response body, so it'll also work with template engines, http proxies etc.
+
+Usage
+-----
+
+```js
+require('express')()
+    .use(require('express-extractheaders')(options))
+    .use(express.static('/path/to/static/files/'));
+```
+
+Example:
+
+```js
+require('express')()
+    .use(require('express-extractheaders')())
+    .use(function (req, res, next) {
+        res.end(
+            '<html>' +
+                '<head>' +
+                    '<meta http-equiv="X-Frame-Options" content="SAMEORIGIN">' +
+                '</head>' +
+                '<body>foo</body>' +
+            '</html>'
+        );
+    }).listen(1337);
+```
+
+```
+$ curl --dump-header - localhost:1337
+HTTP/1.1 200 OK
+X-Powered-By: Express
+Content-Type: text/html; charset=utf-8
+Content-Length: 98
+X-Frame-Options: SAMEORIGIN
+Date: Sun, 27 Jul 2014 12:16:51 GMT
+Connection: keep-alive
+
+<html><head><meta http-equiv="X-Frame-Options" content="SAMEORIGIN"></head><body>foo</body></html>
+```
+
+Installation
+------------
+
+```
+npm install express-extractheaders
+```
+
+Options
+-------
+
+`memoize`: Only extract the response headers once per url. You will probably want to use this option in production as there's a performance hit to parsing all the outgoing HTML.
+
+Security considerations
+-----------------------
+
+Only use this when you trust the downstream middleware. If you proxy to an untrusted host, it will gain the ability to set response headers on the behalf of your server.
+
+License
+-------
+
+express-extractheaders is licensed under a standard 3-clause BSD license -- see the
+`LICENSE` file for details.
---
+++ test/expressExtractHeaders.js
@@ -0,0 +1,158 @@
+/*global describe, it*/
+var unexpected = require('unexpected'),
+    expressExtractHeaders = require('../lib/expressExtractHeaders'),
+    express = require('express'),
+    passError = require('passerror');
+
+describe('expressExtractHeaders', function () {
+    var expect = unexpected.clone()
+        .installPlugin(require('unexpected-express'))
+        .installPlugin(require('unexpected-sinon'));
+
+    it('should specify response headers based on <meta> tags in the response body', function (done) {
+        var responseHtml =
+            '<!DOCTYPE html>\n<html><head><meta http-equiv="Foo" content="Bar"></head><body>foo</body></html>';
+        expect(
+            express()
+                .use(expressExtractHeaders())
+                .use(function (req, res) {
+                    res.send(responseHtml);
+                }),
+            'to be middleware that processes', {
+                request: '/',
+                response: {
+                    headers: {
+                        'Content-Type': 'text/html; charset=utf-8',
+                        Foo: 'Bar'
+                    },
+                    body: responseHtml
+                }
+            },
+            done
+        );
+    });
+
+    it('should specify response headers based on <META> tags in the response body', function (done) {
+        var responseHtml =
+            '<!DOCTYPE html>\n<html><head><META HTTP-EQUIV="Foo" CONTENT="Bar"></head><body>foo</body></html>';
+        expect(
+            express()
+                .use(expressExtractHeaders())
+                .use(function (req, res) {
+                    res.send(responseHtml);
+                }),
+            'to be middleware that processes', {
+                request: '/',
+                response: {
+                    headers: {
+                        'Content-Type': 'text/html; charset=utf-8',
+                        Foo: 'Bar'
+                    },
+                    body: responseHtml
+                }
+            },
+            done
+        );
+    });
+
+    it('should work with HEAD requests', function (done) {
+        expect(
+            express()
+                .use(expressExtractHeaders())
+                .use(function (req, res) {
+                    res.setHeader('Content-Type', 'text/html');
+                    if (req.method === 'HEAD') {
+                        res.end();
+                    } else {
+                        res.end('<!DOCTYPE html>\n<html><head><meta http-equiv="Foo" content="Bar"></head><body>foo</body></html>');
+                    }
+                }),
+            'to be middleware that processes', {
+                request: {
+                    method: 'HEAD',
+                    url: '/'
+                },
+                response: {
+                    headers: {
+                        Foo: 'Bar'
+                    },
+                    body: new Buffer([])
+                }
+            },
+            done
+        );
+    });
+
+    it('should always serve the first extracted set of response headers when memoize:true is given', function (done) {
+        var nextResponseNumber = 1,
+            app = express()
+            .use(expressExtractHeaders({memoize: true}))
+            .use(function (req, res) {
+                res.send('<meta http-equiv="Foo" content="Bar' + nextResponseNumber + '">');
+                nextResponseNumber += 1;
+            });
+        expect(
+            app,
+            'to be middleware that processes', {
+                request: '/',
+                response: {
+                    headers: {
+                        'Content-Type': 'text/html; charset=utf-8',
+                        Foo: 'Bar1'
+                    }
+                }
+            },
+            passError(done, function () {
+                expect(
+                    app,
+                    'to be middleware that processes', {
+                        request: '/',
+                        response: {
+                            headers: {
+                                'Content-Type': 'text/html; charset=utf-8',
+                                Foo: 'Bar1'
+                            }
+                        }
+                    },
+                    done
+                );
+            }));
+    });
+
+    it('should overwrite response headers from the upstream middleware, even when memoizing', function (done) {
+        var app = express()
+            .use(expressExtractHeaders({memoize: true}))
+            .use(function (req, res) {
+                // Currently depends on the upstream middleware playing nice when the header has already been set:
+                if (!res.getHeader('Content-Type')) {
+                    res.setHeader('Content-Type', 'text/html');
+                }
+                res.end('<!DOCTYPE html>\n<html><head><meta http-equiv="Content-Type" content="foo/bar"></head><body>foo</body></html>');
+            });
+        expect(
+            app,
+            'to be middleware that processes', {
+                request: '/',
+                response: {
+                    headers: {
+                        'Content-Type': 'foo/bar'
+                    }
+                }
+            },
+            passError(done, function () {
+                expect(
+                    app,
+                    'to be middleware that processes', {
+                        request: '/',
+                        response: {
+                            headers: {
+                                'Content-Type': 'foo/bar'
+                            }
+                        }
+                    },
+                    done
+                );
+            })
+        );
+    });
+});
